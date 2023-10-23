431b78862450e5d6e4422f0695330ef24cd1c7ca
Dário <147437499+dariobrito01@users.noreply.github.com>
2023-10-23 20:19:21 +0300

    Add files via upload

---
+++ darioatividade09.py
@@ -0,0 +1,7 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+n1 = int(input("digite um número: "))
+n2 = int(input("digite um número: "))
+if  n1 >= n2 :
+    print("o número maior é: " , n1)
+else:
+        print("o número maior é " , n2)
